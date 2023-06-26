8e11fb84b4462f7a775c2d5de93a1141099e2f19
Lindsay Ward <lward23@hotmail.com>
2023-06-26 10:44:36 -0700

    Initial commit with planning

---
+++ planning.rb
@@ -0,0 +1,11 @@
+# Player
+# - state: lives, player_number
+# - behaviour: lose_life
+
+# Question
+# - state: question, answer, already_shown
+# - behaviour: show_question, check_answer
+
+# Game
+# - state: current_player, questions
+# - behaviour: switch_player, make_announcement, show_score, win_game
